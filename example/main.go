13209495957f78236a1a1341a24b4985e694de7a
Napon Mekavuthikul <napon.meka@gmail.com>
2019-04-01 20:44:25 +0700

    bring back the example

--- example/main.go
+++ example/main.go
@@ -8,23 +8,6 @@ import (
 )
 
 func main() {
-	/*
-		"_id": {
-				"$oid": "5c91e115214fc660b6ca650a"
-			},
-			"numbers": [5,6,7],
-			"arr":[{"name":"bone"},{"name":"napon"}],
-			"r" :{
-				"$ref": "<name>",
-				"$id": "<id>"
-			},
-			"x": {
-				"y": "z"
-			},
-			"nickname": "p",
-		"value": 3.14159,
-		"name": "pi5",
-	*/
 	jsonExt := `
 	{
 		"nickname": "p",
@@ -39,6 +22,9 @@ func main() {
 		},
 		"numbers": [5,6,7],
 		"arr":[{"name":"bone"},{"name":"napon"}],
+		"x": {
+			"y": "z"
+		},
 		"a": {
 			"b": {
 				"c": "d"
@@ -56,44 +42,44 @@ func main() {
 			"favorite_count" : 0
 		}
 	}`
-	bsonStr, err := bsonparser.JsonToBsonIndent(jsonExt, "", "    ")
+	bsonStr, err := bsonparser.JsonToBson(jsonExt)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(bsonStr)
 	fmt.Println("==========")
-	// bsonStrInput := `
-	// {
-	// 	"numbers": [
-	// 	  5,
-	// 	  6,
-	// 	  7
-	// 	],
-	// 	"x": {
-	// 	  "y": "z"
-	// 	},
-	// 	"a": {
-	// 	  "b": {
-	// 		"c": "d"
-	// 	  }
-	// 	},
-	// 	"nickname": "p",
-	// 	"value": 3.141590,
-	// 	"_id": ObjectId("5c91e115214fc660b6ca650a"),
-	// 	"arr": [
-	// 	  {
-	// 		"name": "bone"
-	// 	  },
-	// 	  {
-	// 		"name": "napon"
-	// 	  }
-	// 	],
-	// 	"name": "pi5"
-	//   }`
-	// jsonStrOut, err := bsonparser.BsonToJson(bsonStrInput)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(jsonStrOut)
+	bsonStrInput := `
+	{
+		"numbers": [
+		  5,
+		  6,
+		  7
+		],
+		"x": {
+		  "y": "z"
+		},
+		"a": {
+		  "b": {
+			"c": "d"
+		  }
+		},
+		"nickname": "p",
+		"value": 3.141590,
+		"_id": ObjectId("5c91e115214fc660b6ca650a"),
+		"arr": [
+		  {
+			"name": "bone"
+		  },
+		  {
+			"name": "napon"
+		  }
+		],
+		"name": "pi5"
+	  }`
+	jsonStrOut, err := bsonparser.BsonToJson(bsonStrInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(jsonStrOut)
 
 }
